7352b2a3eafdbac9decb5f4150bada4445fcb995
fndsjy <fendysanjaya40@gmail.com>
2025-12-23 10:59:00 +0700

    Revert "Moving CorsOptions from Main to Web"
    This reverts commit d168d2e0a489b72ded144345baa85d9f9b124697.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-// import "cors";
+import "cors";
 
 console.log("TZ =", process.env.TZ);
 console.log("Now =", new Date().toString());
@@ -7,18 +7,14 @@ console.log("Now =", new Date().toString());
 import { web } from "./application/web.js";
 import { logger } from "./application/logging.js";
 
-// const corsOptions = {
-//   origin: [
-//     'http://10.0.1.16:5173',
-//     'http://localhost:5173',
-//     'http://10.0.1.100/',
-// ],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+const corsOptions = {
+  origin: [
+    'http://10.0.1.16:5173',
+    'http://localhost:5173'
+],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true, // penting untuk cookie/JWT
-
-// };
+};
 
 web.listen(5174, "0.0.0.0", () => {
     logger.info("🚀 Flowly Server is running on http://10.0.1.16:5174 and http://localhost:5174");
